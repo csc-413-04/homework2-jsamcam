52458640030878e78a67d481b3ada18cbacc1bc2
Joel Samaniego Campos <jsamani1@mail.sfsu.edu>
2018-10-19 17:36:30 -0700

    Add Maven POM file and two basic classes for the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,55 @@
 package main.java;
 import static spark.Spark.*;
+import com.mongodb.*;
+import com.google.gson.*;
+
+/*
+
+I found a tutorial here https://dzone.com/articles/building-simple-restful-api
+https://blog.openshift.com/developing-single-page-web-applications-using-java-8-spark-mongodb-and-angularjs/
+
+Mongodb java documentation  https://mongodb-documentation.readthedocs.io/en/latest/ecosystem/tutorial/getting-started-with-java-driver.html
+
+*/
+
 
 public class Main {
 
+    //collections for storing all users and all authorization tokens
+    java.util.Map<String, Object> users;
+    java.util.Map<String, Object> auth;
+    private final DB mongoDatabase = null;
+    private final DBCollection collection = mongoDatabase.getCollection("users");
+
+
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
+      staticFiles.externalLocation("public");
       // http://sparkjava.com/documentation
       port(1234);
       // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
 
-    }
-}
+
+      User newUser = new User();
+
+      System.out.println(newUser.getUserName());
+
+      get("/hello", (req, res) -> "Hello Worlds");
+
+      path("/newuser", () -> {
+
+          get("", (req, res) -> "Getting user...");
+      });
+
+      path("/addfriend", () -> {
+          //post("/", null);
+          get("", (req, res) -> "Adding friend...");
+      });
+
+      path("/friends", () -> {
+          get("", (req, res ) -> "Getting friends...");
+
+      });
+
+
+    }//end main method
+}//end Main class
